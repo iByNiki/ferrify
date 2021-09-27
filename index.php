28f4e9c6140421ac95d81df426589a2966e7b015
iByNiki <39004156+iByNiki@users.noreply.github.com>
2021-09-27 19:20:42 +0200

    new index working

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Music app</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
@@ -83,3 +93,8 @@
   </div>
   
 </div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
